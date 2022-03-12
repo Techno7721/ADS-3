a1c80169093ac9e2bfe02efd4264c43a9c13dd7c
Осинин Ярослав <99194733+Techno7721@users.noreply.github.com>
2022-03-12 15:35:57 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,31 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int count = 0;
+  int i = 0;
+  int j = size - 1;
+  while (i < j) {
+        int mid = i + (j - i) / 2;
+        if (arr[mid] == value) {
+            count += 1;
+            for (int k = mid - 1;k >= 0;--k) {
+                if (arr[k] != value) {
+                    break;
+                }
+                count += 1;
+            }
+            for (int k = mid + 1;k < size;++k) {
+                if (arr[k] != value) {
+                    break;
+                }
+                count += 1;
+            }
+            return count;
+        } else if (arr[mid] > value) {
+            j = mid;
+        } else {
+            i = mid + 1;
+        }
+    }
+    return count;
 }
